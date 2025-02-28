ce243bfc67acf4c8c42b675fdd2306b6f3c3468a
MeganKullu <kullumegan@gmail.com>
2025-02-28 15:03:14 +0300

    reverted to initial commit

--- src/main/java/com/example/walletdemo/config/SecurityConfig.java
+++ src/main/java/com/example/walletdemo/config/SecurityConfig.java
@@ -60,7 +60,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("https://walletdemofrontend1.vercel.app")); // Frontend URL
+        configuration.setAllowedOrigins(List.of("http://localhost:3000"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "X-Requested-With"));
         configuration.setAllowCredentials(true);
